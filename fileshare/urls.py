a5135b6434cb536874a689d5bf24de6d26b798b8
Akash R Chandran <chandranrakash@gmail.com>
2024-04-10 01:00:53 +0530

    REFACTOR: Removed admin interface

--- fileshare/urls.py
+++ fileshare/urls.py
@@ -19,7 +19,7 @@ from django.urls import include, path
 from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    # path("admin/", admin.site.urls),
     path("api/", include("api.urls")),
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     # Optional UI:
